4159ca5577ebedede1ba5330c0631dc4e7ef56d8
Yann Derré <55207864+derreyann@users.noreply.github.com>
2021-11-02 11:06:22 +0100

    profile.php réparé
    on fait un blob pour mettre des photos si on a le temps?

--- profile.php
+++ profile.php
@@ -5,13 +5,8 @@
 <?php
 include("menu.php");
 include("BDD.php");
-<<<<<<< Updated upstream
-$sql="SELECT prenom FROM utilisateur WHERE mail=\"".$_SESSION["mail"]."\" AND MdP=PASSWORD(\"".$_SESSION["mdp"]."\");";
-=======
-session_start();
 
 $sql="SELECT * FROM utilisateur WHERE mail=\"".$_SESSION["mail"]."\" AND MdP=PASSWORD(\"".$_SESSION["mdp"]."\");";
->>>>>>> Stashed changes
 $result=$conn->query($sql);
 $row = $result->fetch_assoc();
 ?>
@@ -19,14 +14,7 @@ $row = $result->fetch_assoc();
 </head>
 <h1>Bienvenue sur votre profil <?php echo $row["prenom"] ?></h1>
 
-// Bouton déconnection (clear session)
-<a href="deconnexion.php">Déconnection</a>
-    
-<?php
-echo "Hello World!";
-?>
-<<<<<<< Updated upstream
-	<button onclick="window.location.href = 'modifier_profile.php';">Modifier votre profile</button>
+<button onclick="window.location.href = 'modifier_profile.php';">Modifier votre profile</button>
 
 <body>
 <form id="form" name="form" method="post">
@@ -37,27 +25,26 @@ echo "Hello World!";
  // penser à mettre blob photo profil dans dbb pour pouvoir avoir truc joil
 <table>
 <tr>
-<td>Prénom :</td><td><?php echo $row[prenom]; ?></td>
+<td>Prénom :</td><td><?php echo $row["prenom"]; ?></td>
 </tr>
 <tr>
-<td>Nom :</td><td><?php echo $row[nom]; ?></td>
+<td>Nom :</td><td><?php echo $row["nom"]; ?></td>
 </tr>
 <tr>
-<td>Email :</td><td><?php echo $row[email]; ?></td>
+<td>Email :</td><td><?php echo $row["mail"]; ?></td>
 </tr>
 <tr>
-<td>Date de Naissance:</td><td><?php echo $row[date]; ?></td>
+<td>Date de Naissance:</td><td><?php echo $row["naissance"]; ?></td>
 </tr>
 <tr>
-    //id 
 <?php
-if($row[type_compte] == 0){
+if($row["type_compte"] == 0){
          $type = "Administrateur";
     }
-       elseif($row[type_compte] == 1){
+       elseif($row["type_compte"] == 1){
          $type = "Membre";
        }
-        elseif($row[type_compte] == 2){
+        elseif($row["type_compte"] == 2){
          $type = "Invité";
         }
     ?>
@@ -66,7 +53,6 @@ if($row[type_compte] == 0){
 </tr>
 <tr>
 </table>
-    
->>>>>>> Stashed changes
+
 </body>
 </html>
