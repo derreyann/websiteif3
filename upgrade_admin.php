38cc78e5e8e818132531f33cc61ca70c08d8b16b
Placard <31778398+estebanbecker@users.noreply.github.com>
2021-11-07 14:59:50 +0100

    Verifie admin dans upgrade_admin.php

--- upgrade_admin.php
+++ upgrade_admin.php
@@ -9,7 +9,18 @@
 <?php
     include "BDD.php";
     include "menu.php";
-    //Upgrade admin the user with the id
+    //check if the user is admin
+    if(!isset($_SESSION['mail'])){
+        header("Location:index.php?message=Veuillez vous connecter");
+    }
+    $sql="SELECT * FROM utilisateur WHERE mail=\"".$_SESSION["mail"]."\" ;";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    if(!$row['type_compte']==0){
+        header("Location:index.php?message=Vous n'êtes pas admin.");
+    }
+
+    //Upgrade to admin the user with the id
     $id = $_GET['id'];
     $sql = "UPDATE utilisateur SET type_compte=0 WHERE id = $id";
     $result = $conn->query($sql);
